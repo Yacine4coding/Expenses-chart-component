637fbe73901ed00994619b22e5bcedfba169d6c9
yacine <yacinbaskateur@gmail.com>
2022-06-27 19:42:00 +0100

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+let Bars = document.querySelectorAll('.bar');
+
+const TotHeight = '117';
+const Totamount = '50';
+
+fetch('./data.json')
+.then(response => response.json())
+.then(data =>{
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        Bars.forEach(bar => {
+            if(bar.dataset.day == element.day){
+                let amount = element.amount
+                let total = (amount * TotHeight) / Totamount;;
+                bar.style.height = `${total}px`;
+                bar.previousElementSibling.textContent = `$${amount}`
+                if(amount >= 50){
+                    bar.className = 'HeightBar';
+                }
+            } 
+        });   
+    }
+});
